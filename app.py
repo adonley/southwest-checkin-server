0b1a2406b0b8a2ab5fffa61aa5192fea41ec86ab
Andrew Donley <andrewjdonley@gmail.com>
2019-09-15 17:33:27 -0700

    Add deps. Routes.

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/confirmation', methods=['POST'])
+def submit_confirmation():
+    return
+
+
+@app.route('/confirmation', methods=['GET'])
+def get_confirmations():
+    return
+
+
+@app.route('/confirmation', methods=['DELETE'])
+def delete_confirmation():
+    return
+
+
 if __name__ == '__main__':
     app.run()
