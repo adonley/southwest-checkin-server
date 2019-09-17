4c2e4dd1fe4b6f70a70ce0da32a00cab1daa14cc
Andrew Donley <andrewjdonley@gmail.com>
2019-09-16 19:22:31 -0700

    Working on hooking up the frontend to the backend.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CheckinService } from "../checkin.service";
 import {Observable} from "rxjs";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -9,23 +10,46 @@ import {Observable} from "rxjs";
 })
 export class HomeComponent implements OnInit {
 
+  public checkinSendForm: FormGroup;
+  public checkinGetForm: FormGroup;
+
   constructor(
+    public formBuilder: FormBuilder,
     public checkinService: CheckinService
   ) { }
 
   ngOnInit() {
+    this.checkinSendForm = this.formBuilder.group({
+      'confirmation': ['', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]],
+      'firstName': ['', [Validators.required, Validators.minLength(2)]],
+      'lastName': ['', [Validators.required, Validators.minLength(2)]],
+      'phone': ['', [Validators.minLength(10), Validators.maxLength(10)]],
+      'email': ['', [Validators.required]],
+    });
+
+    this.checkinGetForm = this.formBuilder.group({
+      'confirmation': ['', [Validators.required, Validators.minLength(7), Validators.maxLength(7)]]
+    });
   }
 
   public deleteCheckin() {
+    this.checkinService.deleteCheckin().subscribe((c: Observable<Checkin>) => {
 
+    }, (err: Error) => {
+
+    });
   }
 
   public getCheckin() {
-    
+    this.checkinService.getCheckin().subscribe((c: Observable<Checkin>) => {
+
+    }, (err: Error) => {
+
+    });
   }
 
   public onSubmitCheckin() {
-    this.checkinService.createCheckin().subscribe((s: Observable<Checkin>) => {
+    this.checkinService.createCheckin().subscribe((c: Observable<Checkin>) => {
 
     }, (err: Error) => {
 
